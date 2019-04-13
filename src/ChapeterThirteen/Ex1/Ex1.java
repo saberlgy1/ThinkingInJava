3ec19ad15c263e56c2e92f611c6a97b4d77af6f7
刘光远 <liuguangyuan@neusoft.com>
2019-04-13 10:44:46 +0800

    add Chapter String

---
+++ src/ChapeterThirteen/Ex1/Ex1.java
@@ -0,0 +1,37 @@
+package ChapeterThirteen.Ex1;
+
+/**
+ * @program: ThinkingInJava
+ * @description:
+ * @author: lgy
+ * @create: 2019-04-12 20:14
+ **/
+
+class WaterSource {
+    private String s;
+    WaterSource() {
+        System.out.println("WaterSource()");
+        s = "Constructed";
+    }
+    public String toString() { return s + super.toString(); }
+}
+public class Ex1 {
+    private String valve1, valve2, valve3, valve4;
+    private WaterSource source = new WaterSource();
+    private int i;
+    private float f;
+    // appears to create only one StringBuilder: (using javap -c)
+    public String toString() {
+        return
+                "valve1 = " + valve1 + " " +
+                        "valve2 = " + valve2 + " " +
+                        "valve3 = " + valve3 + " " +
+                        "valve4 = " + valve4 + " " +
+                        "i = " + i + " " + "f = " + f + " " +
+                        "source = " + source;
+    }
+    public static void main(String[] args) {
+        Ex1 sprinklers = new Ex1();
+        System.out.println(sprinklers);
+    }
+}
